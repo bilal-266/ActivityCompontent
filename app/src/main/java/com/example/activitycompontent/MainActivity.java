f553cac4eeaaee7fd6a126070dc467446f7a6a82
Bilal Yousaf <F2017266297@umt.edu.pk>
2021-03-15 15:47:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/activitycompontent/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.activitycompontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("start","Start Method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("resume","Resume Method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("pause","Pause Method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("stop","Stop Method");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("restart","restart Method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("destroy","Destroy Method");
+    }
+}
